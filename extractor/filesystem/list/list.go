29e2b68f3585e4e4092fcdf06bcb15ab954c3288
Gareth Jones <3151613+G-Rath@users.noreply.github.com>
2026-02-24 15:49:24 +1300

    fix: remove `denotssource` from list

--- extractor/filesystem/list/list.go
+++ extractor/filesystem/list/list.go
@@ -51,7 +51,6 @@ import (
 	"github.com/google/osv-scalibr/extractor/filesystem/language/java/pomxmlnet"
 	"github.com/google/osv-scalibr/extractor/filesystem/language/javascript/bunlock"
 	"github.com/google/osv-scalibr/extractor/filesystem/language/javascript/denojson"
-	"github.com/google/osv-scalibr/extractor/filesystem/language/javascript/denotssource"
 	"github.com/google/osv-scalibr/extractor/filesystem/language/javascript/packagejson"
 	"github.com/google/osv-scalibr/extractor/filesystem/language/javascript/packagelockjson"
 	"github.com/google/osv-scalibr/extractor/filesystem/language/javascript/pnpmlock"
@@ -200,7 +199,6 @@ var (
 		packagejson.Name:     {packagejson.New},
 		packagelockjson.Name: {packagelockjson.New},
 		denojson.Name:        {denojson.New},
-		denotssource.Name:    {denotssource.New},
 		pnpmlock.Name:        {pnpmlock.New},
 		yarnlock.Name:        {yarnlock.New},
 		bunlock.Name:         {bunlock.New},
